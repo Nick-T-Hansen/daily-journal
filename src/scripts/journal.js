05119b2af317841d50b89d4e08d950699afe109c
Nick Hansen <nick.hansen13@gmail.com>
2019-01-07 09:35:14 -0600

    updated json server with id's and moved submit button outide of the form html

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,19 +1,11 @@
 //Main application logic that uses the functions and objects defined in the other JavaScript files.
 
-/* -------------------- TO DO ----------------------------
-POST is not working
--information submitted in the form does not post to the server 
--click on button does work
--no errors in console
--page looks like it is refreshing to its prior state when click is hit (is this a possibility?)
-*/
-
 
 API.getJournalEntries()
     .then(entriesDOM.renderJournalResponse)
     
 
-//creates an event listener which takes the form values, turns it into an object, and posts it to the JSON
+//TO DO: creates an event listener which takes the form values, turns it into an object, and posts it to the JSON (building button listener function for submit button on line 31, have not tested.
 const eventListeners = {
     journalEntryAppendToJSON () {
         let resetJournalEntriesQuery = document.querySelector(".entryLog")
@@ -35,9 +27,13 @@ const eventListeners = {
                 entriesDOM.renderJournalResponse()
             });
         
+    },
+    //add the event listener to the submit buttonS
+    onBottonClick (){
+        let submitButton = document.querySelector("#submitButton")
+        submitButton.addEventListener("click", eventListeners.journalEntryAppendToJSON)
     }
 };
 
-
-
+eventListeners.onBottonClick ();
 
